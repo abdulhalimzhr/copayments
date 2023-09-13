86fc3e517859a89c7b0ef4f2f2877afeb07ab327
abdulhalimzhr <sideloadoel@icloud.com>
2023-09-13 12:06:02 +0700

    add home

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,6 @@
-<div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+<div class="container h-80">
+    <div class="flex justify-center p-10 align-middle">
+        <h1 class="text-4xl font-semibold">CoPayments</h1>
+        <sub class="text-sm font-semibold">v1.0</sub>
+    </div>
 </div>
